56a65a1863e4f3f7e847306b0993686b69e8c302
Ggross98 <31427821+Ggross98@users.noreply.github.com>
2025-05-19 17:25:59 +0800

    Delete useless files

--- RhythmLane/Library/PackageCache/com.unity.ai.navigation@1.1.5/Editor/ConversionSystem/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-#if UNITY_2022_2_OR_NEWER
-using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("Unity.AI.Navigation.Updater")]
-#endif
--- RhythmLane/Library/PackageCache/com.unity.ai.navigation@1.1.5/Editor/Updater/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-#if UNITY_2022_2_OR_NEWER
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.AI.Navigation.Editor.Tests")]
-#endif
